ff1389e1ccac337dd9cfdb1761e8f1245fcb0483
Twin_Notebook <michael.wiktorin@gmail.com>
2026-06-17 17:25:25 +0200

    Add initial project files for time tracking dashboard
    Includes project setup with references to challenge details, data structure in JSON, and initial HTML template. Added visual assets for activity icons and AGENTS.md to define AI assistant behavior.

---
+++ resources/views/components/icons/ellipsis.blade.php
@@ -0,0 +1,7 @@
+<svg
+    {{ $attributes->merge(["class" => ""]) }}
+    xmlns="http://www.w3.org/2000/svg">
+    <path
+        d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z"
+        fill="currentColor" fill-rule="evenodd"/>
+</svg>
