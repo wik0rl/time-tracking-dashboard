d6bf78597b5b564d3cefd6f5eb8a7eb64b4d85d8
michaelwiktorin <michael.wiktorin@gmail.com>
2026-06-18 22:01:20 +0200

    Update Vercel.json to set `/tmp/views` for compiled views and adjust environment variables

--- api/index.php
+++ api/index.php
@@ -1,4 +1,7 @@
 <?php
 
-// Forward Vercel requests to normal index.php
+// Vercel hat kein beschreibbares Filesystem
+$_ENV['APP_STORAGE'] = '/tmp';
+
+// Symlinks funktionieren nicht – public/storage muss anders gelöst werden
 require __DIR__ . '/../public/index.php';
