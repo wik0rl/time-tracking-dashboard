4ef0655980cf20dc7ff8f20af299559f67bcce7e
michaelwiktorin <michael.wiktorin@gmail.com>
2026-06-18 22:38:39 +0200

    new try...

--- api/index.php
+++ api/index.php
@@ -1,12 +1,3 @@
 <?php
 
-// Vercel's filesystem ist read-only – alles Beschreibbare muss nach /tmp
-$_ENV['VIEW_COMPILED_PATH'] = '/tmp/views';
-$_SERVER['VIEW_COMPILED_PATH'] = '/tmp/views';
-
-// /tmp/views Verzeichnis erstellen falls nicht vorhanden
-if (!is_dir('/tmp/views')) {
-    mkdir('/tmp/views', 0755, true);
-}
-
 require __DIR__ . '/../public/index.php';
